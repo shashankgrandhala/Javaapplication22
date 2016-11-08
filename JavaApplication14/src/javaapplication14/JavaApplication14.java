e78b21f95df383c4b5c28be31948855da8d74ccd
shashankgrandhala <shashankgrandhala@gmail.com>
2016-11-08 09:27:11 -0600

    add promptuser method

--- JavaApplication14/src/javaapplication14/JavaApplication14.java
+++ JavaApplication14/src/javaapplication14/JavaApplication14.java
@@ -16,6 +16,9 @@ public class JavaApplication14 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
     }
-    
+    public void promptUser(){
+            
+            }
 }
