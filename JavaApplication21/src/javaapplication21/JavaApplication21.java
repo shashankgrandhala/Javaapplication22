5f3bb40b896395befcd6f420d7b5dbe8e1843148
shashankgrandhala <shashankgrandhala@gmail.com>
2016-11-08 14:25:40 -0600

    added mem

--- JavaApplication21/src/javaapplication21/JavaApplication21.java
+++ JavaApplication21/src/javaapplication21/JavaApplication21.java
@@ -16,6 +16,8 @@ public class JavaApplication21 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int num; 
+        int sum;
     }
     
 }
